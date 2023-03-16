b80cf18b9876162154cf9b82b444d4d17878f5e2
Wycliffe <wycliffekiprono4@gmail.com>
2023-03-16 15:53:39 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+/* function foo() {
+    return 'bar';
+}
+
+const foo = function() {
+    return 'bar';
+}
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
+
+const sum = (parameter1, parameter1) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}
+sum(1,2); 
+
+const nums = [1,2,3];
+const squares = nums.map(x => x ** 2);
+squares;
+nums;
+
+finishedItems = overdueTodoItems.map( item => item.className = "complete");
+Headers.innerText = `You finished ${finished.length} items!`; */
+
+function divide(x = 2000,y = 100) {
+    return Math.floor(x/y);
+}
+divide();
+
+
+const square = x => (x*x);
+square;
+
+const add = (x,y) => (x+y);
+add;
